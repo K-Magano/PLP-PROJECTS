114dfc397e2b762f419aba7188db67ccb20b1fc0
Keorapetse Magano <124664588+K-Magano@users.noreply.github.com>
2024-05-29 07:39:46 +0200

    added new files

---
+++ Dart with Flutter/main.dart
@@ -0,0 +1,15 @@
+void main() {
+  //Declaring
+  String name = "Keo";
+  String address = "70 Mellvile";
+  num age = 34;
+  num height = 5.9;
+  bool isMarried = false;
+
+//Printing/Output
+  print("Name is $name");
+  print("Address is $address");
+  print("Age is $age");
+  print("Height is $height");
+  print("Married status is $isMarried");
+}
